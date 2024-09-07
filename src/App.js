0954fe4fd345aba262b39fae7852eb8bd5e4a9ed
cho4u4o <cho4u4o@gmail.com>
2024-09-07 14:20:33 +0900

    Feat : 입력 단계 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const names = await this.getCarname();
+    const times = await this.getTimes();
+  }
+  async getCarname() {
+    try {
+      const input = await Console.readLineAsync(
+        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+      );
+      const names = Array.from(input.split(",").map((name) => name.trim()));
+      return names;
+    } catch (error) {
+      // reject 되는 경우
+    }
+  }
+
+  async getTimes() {
+    try {
+      const input = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+      const times = parseInt(input);
+      return times;
+    } catch (error) {
+      // reject 되는 경우
+    }
+  }
 }
 
 export default App;
